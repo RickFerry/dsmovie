ea41279c2903a5af9e55c1f1b56fd37d2c7987d6
Ferry <rick_ferry1@hotmail.com>
2022-05-23 23:22:55 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/ferry/dsmovie/entities/Movie.java
@@ -0,0 +1,35 @@
+package com.ferry.dsmovie.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(name = "tb_movie")
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+	
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
+	
+	public Movie() {
+	}
+}
