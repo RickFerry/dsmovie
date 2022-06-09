1553b25b39462c73f6157d690bff78c9024f1c54
Ferry <rick_ferry1@hotmail.com>
2022-06-09 20:29:05 -0300

    useStatenuseEffect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,16 +1,24 @@
 import axios from "axios";
 import MovieCard from "components/moviecard";
 import Pagination from "components/pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies`).then(resp => {
-        console.log(resp.data)
-    });
+    const [pageNumber, setpageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`).then(resp => {
+            const data = resp.data as MoviePage
+            setpageNumber(data.number);
+        });
+    },[])
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
