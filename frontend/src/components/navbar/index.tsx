f3a44a1179d86355864a68e3c89b78c7a5f062ef
Ferry <rick_ferry1@hotmail.com>
2022-06-11 00:29:30 -0300

    My GitHub

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -8,10 +8,10 @@ function Navbar() {
     <nav className="container">
         <div className="dsmovie-nav-content">
             <h1>DSMovie</h1>
-            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+            <a href="https://github.com/RickFerry" target="_blank" rel="noreferrer">
                 <div className="dsmovie-contact-container">
                     <GitHubIcon />
-                    <p className="dsmovie-contact-link">/devsuperior</p>
+                    <p className="dsmovie-contact-link">/RickFerry</p>
                 </div>
             </a>
         </div>
